e5d8b80f6faba75a69126dca8acd239cee3f025b
Alexander Wheadon <alexanderwheadon@gmail.com>
2015-02-21 15:36:44 -0500

    Update and rename Source to Source/FormatString.java

---
+++ Source/FormatString.java
@@ -0,0 +1,4 @@
+String BestCaniadates = null;
+		String TempHoldSubStrings[]= new String[20];
+		TempHoldSubStrings = BestCaniadates.split(" ");
+		String output = TempHoldSubStrings[0] + "-" + TempHoldSubStrings[1] + " " + TempHoldSubStrings[2];
